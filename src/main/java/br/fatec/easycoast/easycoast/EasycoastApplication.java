5be82e813bbed9c0ad00c850069cc8c8b765c530
glaucotodesco <glauco.todesco@gmail.com>
2024-11-08 14:32:37 -0300

    first commit

---
+++ src/main/java/br/fatec/easycoast/easycoast/EasycoastApplication.java
@@ -0,0 +1,13 @@
+package br.fatec.easycoast.easycoast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasycoastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasycoastApplication.class, args);
+	}
+
+}
