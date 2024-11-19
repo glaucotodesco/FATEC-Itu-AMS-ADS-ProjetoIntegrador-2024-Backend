85b914e2f63275755b9306aa1413d5fab303070b
JuliaCanavezzi <jucanavezzi03@gmail.com>
2024-11-19 17:48:13 -0300

    :sparkles: feat: GetMapping and GetMapping by Id

--- src/main/java/br/fatec/easycoast/resources/CategoryController.java
+++ src/main/java/br/fatec/easycoast/resources/CategoryController.java
@@ -2,8 +2,11 @@ package br.fatec.easycoast.resources;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import br.fatec.easycoast.dtos.CategoryResponse;
 import br.fatec.easycoast.entities.Category;
 import br.fatec.easycoast.services.CategoryService;
 
@@ -14,9 +17,14 @@ public class CategoryController {
   private CategoryService service;
 
   @GetMapping("categories")
-  public List <Category> getCategories(){
-    return service.getCategories();
+  public ResponseEntity <List<CategoryResponse>> getCategories(){
+    return ResponseEntity.ok(service.getCategories());
   }
+
+   @GetMapping("categories/{id}")
+    public ResponseEntity <CategoryResponse> getCategoryById(@PathVariable int id) {
+        return ResponseEntity.ok(service.getCategoryById(id));   
+    }
 
   
 }
