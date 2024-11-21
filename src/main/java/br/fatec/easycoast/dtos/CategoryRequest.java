e196584f8bab7dab8c5f41deb2b413c2518b5835
canavezzi <161068647+JuliaCanavezzi@users.noreply.github.com>
2024-11-21 15:31:22 -0300

    Revert "✨ feat: GetMapping and GetMapping by Id"

--- src/main/java/br/fatec/easycoast/dtos/CategoryRequest.java
+++
@@ -1,9 +0,0 @@
-package br.fatec.easycoast.dtos;
-
-public record CategoryRequest(
-
-String name,
-Boolean availability
-) {
-  
-}
--- src/main/java/br/fatec/easycoast/repositories/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package br.fatec.easycoast.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.fatec.easycoast.entities.Category;
-
-public interface CategoryRepository extends JpaRepository <Category, Integer>{
-  
-}
