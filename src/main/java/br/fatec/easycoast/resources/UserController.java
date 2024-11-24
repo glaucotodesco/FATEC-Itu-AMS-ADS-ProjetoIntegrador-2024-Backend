0ce3cdc3d25fb9c120f159f13ed67e761fcf3bbf
João Otávio <jofs2006@gmail.com>
2024-11-23 21:56:14 -0300

    ✨ feat: rename folder controller to resources

--- src/main/java/br/fatec/easycoast/resources/UserController.java
+++ src/main/java/br/fatec/easycoast/resources/UserController.java
@@ -1,10 +1,13 @@
-package br.fatec.easycoast.controller;
+package br.fatec.easycoast.resources;
 
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.fatec.easycoast.dtos.UserRequest;
