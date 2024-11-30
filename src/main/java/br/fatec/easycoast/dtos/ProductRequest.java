e70a8dc67470f417768d441a94040609a8aacef8
AngeloModanez <angelomodanez15@gmail.com>
2024-11-30 20:57:43 -0300

    :sparkles: feat: Product Validations

--- src/main/java/br/fatec/easycoast/dtos/ProductRequest.java
+++ src/main/java/br/fatec/easycoast/dtos/ProductRequest.java
@@ -1,10 +1,34 @@
 package br.fatec.easycoast.dtos;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 public record ProductRequest(
-  String name,
-  String description,
-  Float price,
-  Float discount,
-  Boolean availability,
-  String category
-) {}
+    @NotBlank(message = "Product name cannot be blank") 
+    @Size(min = 3, message = "Minimum length for the name is 3 characters") 
+    String name,
+
+    @NotBlank(message = "Description cannot be blank") 
+    @Size(min = 10, message = "Minimum length for the description is 10 characters") 
+    String description,
+
+    @NotNull(message = "Price cannot be null") 
+    @Positive(message = "Price must be positive") 
+    float price,
+
+    @NotNull(message = "Discount cannot be null") 
+    @Min(value = 0, message = "Discount must be at least 0") 
+    @Max(value = 100, message = "Discount cannot be more than 100") 
+    float discount,
+
+    @NotNull(message = "Availability cannot be null") 
+    Boolean availability,
+
+    @NotBlank(message = "Category cannot be blank") 
+    String category
+    ) {
+}
