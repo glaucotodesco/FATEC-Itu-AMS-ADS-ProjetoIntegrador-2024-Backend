72dbdbcf732cb4707806033de7838478404009c3
vinisvp <viniciuspedro0905@gmail.com>
2025-08-19 14:16:11 -0300

    :sparkles: feat: Handling database exception

--- src/main/java/br/fatec/easycoast/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/fatec/easycoast/resources/exceptions/ResourceExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import br.fatec.easycoast.services.exceptions.DatabaseException;
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
@@ -15,20 +16,55 @@ public class ResourceExceptionHandler {
     //It will run when a validation fails, e return an error for the request of the API
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ValidationError> validationException(MethodArgumentNotValidException exception, HttpServletRequest request){
+        //Create a validation error
         ValidationError error = new ValidationError();
         
+        //Get the status code
         HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
 
+        //Set the error text
         error.setError("Validation Error");
+        //Set the error messsage
         error.setMessage(exception.getMessage());
+        //Set the path of the error
         error.setPath(request.getRequestURI());
+        //Set the status code
         error.setStatus(status.value());
+        //Set the time when it happened
         error.setTimeStamp(Instant.now());
 
+        //Get the result of the exception
         exception.getBindingResult()
+                 //Get the validation erros
                  .getFieldErrors()
+                 //For each error, put the messagem in the validation error
                  .forEach(e -> error.addError(e.getDefaultMessage()));
 
+        //Return the validation error in the body of the response
+        return ResponseEntity.status(status).body(error);
+    }
+
+    //It will run when there is a DatabaseException, e return an error for the request of the API
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> databaseException(MethodArgumentNotValidException exception, HttpServletRequest request){
+        //Create a standard error
+        StandardError error = new StandardError();
+        
+        //Get the status code
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        //Set the error text
+        error.setError("Database Error");
+        //Set the error messsage
+        error.setMessage(exception.getMessage());
+        //Set the path of the error
+        error.setPath(request.getRequestURI());
+        //Set the status code
+        error.setStatus(status.value());
+        //Set the time when it happened
+        error.setTimeStamp(Instant.now());
+
+        //Return the standard error in the body of the response
         return ResponseEntity.status(status).body(error);
     }
 }
