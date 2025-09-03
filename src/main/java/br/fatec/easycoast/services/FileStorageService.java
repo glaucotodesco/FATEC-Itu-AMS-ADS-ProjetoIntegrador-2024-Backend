1826922b3b105be486aa48ca6678cd0db63ba81f
Pedro Vinincius <viniciuspedro0905@gmail.com>
2025-09-02 21:27:17 -0300

    :construction: feat: Added FileStorageService

---
+++ src/main/java/br/fatec/easycoast/services/FileStorageService.java
@@ -0,0 +1,78 @@
+package br.fatec.easycoast.services;
+
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Service
+public class FileStorageService {
+    private final Path rootLocation;
+
+	public FileStorageService() {
+        this.rootLocation = Paths.get("images");
+	}
+    
+    public void store(MultipartFile file) {
+		try {
+			if (file.isEmpty()) {
+				throw new IllegalArgumentException("Failed to store empty file.");
+			}
+			Path destinationFile = this.rootLocation.resolve(
+					Paths.get(file.getOriginalFilename()))
+					.normalize().toAbsolutePath();
+			if (!destinationFile.getParent().equals(this.rootLocation.toAbsolutePath())) {
+				// This is a security check
+				throw new IllegalArgumentException(
+						"Cannot store file outside current directory.");
+			}
+			try (InputStream inputStream = file.getInputStream()) {
+				Files.copy(inputStream, destinationFile,
+					StandardCopyOption.REPLACE_EXISTING);
+			}
+		}
+		catch (IOException e) {
+			throw new IllegalArgumentException("Failed to store file.", e);
+		}
+	}
+
+    public Path load(String filename) {
+		return rootLocation.resolve(filename);
+	}
+
+    public Resource loadAsResource(String filename) {
+		try {
+			Path file = load(filename);
+			Resource resource = new UrlResource(file.toUri());
+			if (resource.exists() || resource.isReadable()) {
+				return resource;
+			}
+			else {
+				throw new IllegalArgumentException(
+						"Could not read file: " + filename);
+
+			}
+		}
+		catch (MalformedURLException e) {
+			throw new IllegalArgumentException("Could not read file: " + filename, e);
+		}
+	}
+
+    public void init() {
+		try {
+			Files.createDirectories(rootLocation);
+		}
+		catch (IOException e) {
+			throw new IllegalArgumentException("Could not initialize storage", e);
+		}
+	}
+}
