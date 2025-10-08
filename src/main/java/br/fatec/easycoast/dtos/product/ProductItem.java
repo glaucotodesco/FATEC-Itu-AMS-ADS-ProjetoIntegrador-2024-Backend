383d0602fe629e36fe2f66c1150867be2e8621a5
vinisvp <viniciuspedro0905@gmail.com>
2025-10-08 14:55:43 -0300

    :sparkles: feat: Now orderItem has validation on removables

--- src/main/java/br/fatec/easycoast/dtos/product/ProductItem.java
+++ src/main/java/br/fatec/easycoast/dtos/product/ProductItem.java
@@ -7,13 +7,36 @@ import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 
 @Embeddable
-public record ProductItem(
+public class ProductItem {
+    
     @NotNull(message = "Product Item can't be null")
     @ManyToOne
     @JoinColumn(name = "ITEM_ID")
-    Item item,
+    private Item item;
+    
     @NotNull(message = "Item removable can't be null")
-    Boolean removable
-) {
-
+    private Boolean removable;
+    
+    public ProductItem() {}
+    
+    public ProductItem(Item item, Boolean removable) {
+        this.item = item;
+        this.removable = removable;
+    }
+    
+    public Item getItem() {
+        return item;
+    }
+    
+    public void setItem(Item item) {
+        this.item = item;
+    }
+    
+    public Boolean getRemovable() {
+        return removable;
+    }
+    
+    public void setRemovable(Boolean removable) {
+        this.removable = removable;
+    }
 }
